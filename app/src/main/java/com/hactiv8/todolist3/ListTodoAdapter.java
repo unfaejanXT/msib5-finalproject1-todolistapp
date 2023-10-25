4cd51c92df2e2b4bfd615cab98f8eb3226c3d03b
Janwar Ksatria Pamungkas <janwarksatriapamungkas@hotmail.com>
2023-10-25 13:02:12 +0700

    Menambahkan fungsi ListTodoAdapter

--- app/src/main/java/com/hactiv8/todolist3/ListTodoAdapter.java
+++ app/src/main/java/com/hactiv8/todolist3/ListTodoAdapter.java
@@ -1,16 +1,38 @@
 package com.hactiv8.todolist3;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
 public class ListTodoAdapter extends RecyclerView.Adapter<ListTodoAdapter.ListViewHolder> {
+    private List<ListToDoClass> todolist;
+    private Context context;
+
+    public ListTodoAdapter(Context context,List<ListToDoClass> todolist){
+        this.context = context;
+        this.todolist = todolist;
+    }
+
+    public ListTodoAdapter(Context context,List<ListToDoClass> todolist, hapusKegiatanListener hapusListener){
+        this.context = context;
+        this.todolist = todolist;
+        this.hapusListener = hapusListener;
+    }
+
+
     @NonNull
     @Override
     public ListTodoAdapter.ListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_list,parent,false);
+        return new ListViewHolder(view);
     }
 
     @Override
@@ -23,10 +45,33 @@ public class ListTodoAdapter extends RecyclerView.Adapter<ListTodoAdapter.ListVi
         return 0;
     }
 
-    public static class ListViewHolder extends RecyclerView.ViewHolder {
+    public class ListViewHolder extends RecyclerView.ViewHolder {
+        private TextView mtvNamaKegiatan;
+        private ImageButton mbtnHapusKegiatan;
+        private ImageButton mbtnUbahKegiatan;
 
         public ListViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            mtvNamaKegiatan = itemView.findViewById(R.id.tvNamaKegiatan);
+            mbtnUbahKegiatan = itemView.findViewById(R.id.btnUbahKegiatan);
+            mbtnHapusKegiatan = itemView.findViewById(R.id.btnHapuskegiatan);
+
+            mbtnUbahKegiatan.setOnClickListener(v -> {
+
+            });
+
+            mbtnHapusKegiatan.setOnClickListener(v -> {
+                hapusListener.onHapusKegiatan(getAdapterPosition());
+            });
+
+
         }
     }
+
+    public interface hapusKegiatanListener{
+        void onHapusKegiatan(int position);
+    }
+
+    private hapusKegiatanListener hapusListener;
 }
